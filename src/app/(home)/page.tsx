f367ae85a369582f3c615ce9fd9b0906c666547b
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2025-02-06 19:07:30 +0545

    Add shadcn  UI Tabs component and update Home page layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className=" bg-white  ">
-      <div className=" container py-12 mx-auto flex justify-between items-center">
+      <div className=" container py-10 mx-auto flex justify-between items-center">
         <div className=" flex flex-col gap-4">
           <h1 className=" font-semibold text-6xl leading-tight">
             Super Delicious Pizza In <br />
@@ -13,7 +13,7 @@ export default function Home() {
           <p className=" max-w-lg leading-snug">
             Enjoy a free meal if your order takes more than 45 minutes!
           </p>
-          <Button className=" w-fit rounded-full mt-12" size={"lg"}>
+          <Button className=" w-fit rounded-full mt-8" size={"lg"}>
             Get your pizza now!
           </Button>
         </div>
