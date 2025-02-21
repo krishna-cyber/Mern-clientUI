ae06c0cec1627b834385a9abadb7f4d015f3bba9
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2025-02-21 21:47:23 +0545

    Add cartSlice for managing cart state in Redux store

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import cartReducer from "./feature/cartSlice";
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: { cart: cartReducer },
   });
 };
 
