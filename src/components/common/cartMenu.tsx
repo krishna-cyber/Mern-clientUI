87c5d774cb18d217795eecc66aed0247b98ea242
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2025-02-24 22:02:30 +0545

    Update CartMenu to display dynamic cart item count and clean up cartSlice

--- src/components/common/cartMenu.tsx
+++ src/components/common/cartMenu.tsx
@@ -1,8 +1,11 @@
+"use client";
+import { useAppSelector } from "@/lib/store/hooks";
 import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const CartMenu = () => {
+  const cartItems = useAppSelector((state) => state.cart?.cartItems);
   return (
     <Link
       href={"/cart"}
@@ -10,7 +13,7 @@ const CartMenu = () => {
     >
       <ShoppingCart />
       <div className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
-        8
+        {cartItems?.length}
       </div>
     </Link>
   );
